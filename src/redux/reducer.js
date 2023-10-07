7b3818778bb594df7a4fb4899f04398f2f7605a6
Dmytro Huriev <hooch.inbox@gmail.com>
2023-10-08 00:24:16 +0300

    redux edit

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,37 +1,38 @@
-const initialState = {
-  contacts: [
-    { id: 0, name: 'Piter', number: '380938034433' },
-    { id: 1, name: 'Anna', number: '380938034422' },
-    { id: 2, name: 'Master React', number: '380938034419' },
-    { id: 3, name: 'Queen', number: '380938034418' },
-    { id: 4, name: 'Beezz', number: '380938034417' },
-  ],
-  filter: '',
-};
+import { combineReducers } from 'redux';
 
-export const rootReducer = (state = initialState, action) => {
+const contactsInitialState = [
+  { id: 0, name: 'Piter', number: '380938034433' },
+  { id: 1, name: 'Anna', number: '380938034422' },
+  { id: 2, name: 'Master React', number: '380938034419' },
+  { id: 3, name: 'Queen', number: '380938034418' },
+  { id: 4, name: 'Beezz', number: '380938034417' },
+];
+const contactReducer = (state = contactsInitialState, action) => {
   switch (action.type) {
     case 'contacts/addContact': {
-      return {
-        ...state,
-        contacts: [...state.contacts, action.payload],
-      };
+      return [...state, action.payload];
     }
     case 'contacts/remContact': {
-      return {
-        ...state,
-        contacts: state.contacts.filter(
-          contact => contact.id !== action.payload.id
-        ),
-      };
-    }
-    case 'filter/changeFilter': {
-      return {
-        ...state,
-        filter: action.payload.filteredName,
-      };
+      return state.filter(contact => contact.id !== action.payload.id);
     }
     default:
       return state;
   }
 };
+
+const filtersInitialState = '';
+
+const filterReducer = (state = filtersInitialState, action) => {
+  switch (action.type) {
+    case 'filter/changeFilter': {
+      return `${action.payload.filteredName}`;
+    }
+    default:
+      return state;
+  }
+};
+
+export const rootReducer = combineReducers({
+  contacts: contactReducer,
+  filter: filterReducer,
+});
